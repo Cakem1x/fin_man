ea374c8adcb02c2e3a8bf522979d58af4f247ba7
Matthias Holoch <mholoch@gmail.com>
2026-06-13 17:07:35 +0200

    tweak dev tooling

--- flake.nix
+++ flake.nix
@@ -4,17 +4,38 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            # Custom hooks that use justfile recipes for parity
+            just-lint = {
+              enable = true;
+              name = "just-lint";
+              entry = "just lint";
+              files = "\\.go$";
+              pass_filenames = false;
+            };
+          };
+        };
       in
       {
+        checks = {
+          inherit pre-commit-check;
+        };
+
         devShells.default = pkgs.mkShell {
+          inherit (pre-commit-check) shellHook;
+          buildInputs = pre-commit-check.enabledPackages;
+
           packages = with pkgs; [
             # Go toolchain
             delve
